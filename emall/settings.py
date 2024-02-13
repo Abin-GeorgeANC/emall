9c21f9e06c803f7073eb869955dda0b1124a5630
Abin George <abin.george@experionglobal.local>
2024-02-13 14:35:40 +0530

    changed user auth table for more details

--- emall/settings.py
+++ emall/settings.py
@@ -139,6 +139,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+AUTH_USER_MODEL= 'user.User'
 
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
