f45a445c0ac7103ef1cb5ed567ed556d8b8882db
Abin George <abin.george@experionglobal.local>
2024-02-14 13:49:12 +0530

    completed user methods , admin listing users

--- emall/settings.py
+++ emall/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'user',
     'rest_framework',
+     'django_rest_passwordreset',
 
 ]
 
@@ -190,3 +191,6 @@ SIMPLE_JWT = {
     "SLIDING_TOKEN_OBTAIN_SERIALIZER": "rest_framework_simplejwt.serializers.TokenObtainSlidingSerializer",
     "SLIDING_TOKEN_REFRESH_SERIALIZER": "rest_framework_simplejwt.serializers.TokenRefreshSlidingSerializer",
 }
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+#anymail or smtp email to send emails actually 
